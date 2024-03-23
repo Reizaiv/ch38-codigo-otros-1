4c3f32eca71b41bef2e91157a0c1aee0d98e4a35
Javier Villagomez <javier_diaz16@hotmail.com>
2024-03-22 18:19:06 -0600

    nuevas correciones al codigo, mejorada legibilidad

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,25 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
 console.log(usersEndpoint);
-const $n = document.querySelector("#name"); //4
-const $b = document.querySelector("#blog"); //4
-const $l = document.querySelector("#location"); //4
+const userName = document.querySelector("#name"); //4, 5
+const blogName = document.querySelector("#blog"); //4, 5
+const userLocation = document.querySelector("#location"); //4, 5
 
 const displayUser = async (username) => { //1, 2
-  $n.textContent = 'cargando...';
+  userName.textContent = 'cargando...'; //5
   const response = await fetch(`${usersEndpoint}/${username}`);
   //console.log(`${usersEndpoint}/${username}`)
   const data = await response.json(); //Faltaba una variable data que reciba el obejto a partir del JSON
   console.log(data);
-  $n.textContent = `${data.name}`; //3
-  $b.textContent = `${data.blog}`; //3
-  $l.textContent = `${data.location}`; //3
+  userName.textContent = `${data.name}`; //3, 5
+  blogName.textContent = `${data.blog}`; //3, 5
+  userLocation.textContent = `${data.location}`; //3, 5
 }
 
 const handleError = (err) => { //1
   console.log('OH NO!');
   console.log(err);
-  $n.textContent = `Algo salió mal: ${err}`
+  userName.textContent = `Algo salió mal: ${err}` //se homogeneizo el nombre de la variable
 }
 
 displayUser('stolinski').catch(handleError);
@@ -29,4 +29,5 @@ displayUser('stolinski').catch(handleError);
  * 2. Se agrega la palabra async a la funcion para indicar que es una funcion asincrona
  * 3. se cambiaron comillas "" por ``
  * 4. Se utiliza # para identificar los elementos por el id
+ * 5. Se les da a las variables un nombre mas legible y se elimina el $
  */
